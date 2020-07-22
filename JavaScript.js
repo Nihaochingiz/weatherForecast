33f827d153f38ef98a8bda4a18853571f9b4c8e6
Nihaochingiz <66513936+Nihaochingiz@users.noreply.github.com>
2020-07-22 22:01:22 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+var days =["Monday","Tuesday","Wednesday","Thursday","Friday"];
+var weather = ["Sunny","Partly Sunny","Partly Cloudy",
+"Cloudy","Raining","Snowing","Thunderstorm","Foggy"];
+var maxTemp = 100;
+var minTemp = 0;
+
+generateWeather();
+
+function generateWeather() {
+for (var i = 0; i< days.length;i++) {
+var weatherToday = weather[Math.floor(Math.random() *
+weather.length)];
+var tempToday = Math.floor(Math.random() * (maxTemp-minTemp)+minTemp);
+document.getElementById("5DayWeather").innerHTML+="<div id='" + days [i] + "'class='"+weatherToday+"'><b>Forecast for"+ days [i] + ":</b><br><br>"+weatherToday+"and"+
+tempToday + "degrees.</div>";
+}
+}
